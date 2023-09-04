fb7e52227fd2c8d9a3ccf3a09a48a7c3403d0110
sujithdusa-stride <sujith.kumar@stride.ai>
2023-09-04 18:43:11 +0530

    version 0.1.2

--- setup.py
+++ setup.py
@@ -7,15 +7,18 @@ here = os.path.abspath(os.path.dirname(__file__))
 with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
     long_description = "\n" + fh.read()
 
-VERSION = '1.0'
-DESCRIPTION = 'Streaming video data via networks'
-LONG_DESCRIPTION = 'A package that allows to build simple streams of video, audio and camera data.'
+VERSION = '0.1.2'
+DESCRIPTION = 'Stride Licensing module for internal tools'
+LONG_DESCRIPTION = 'A package that allows to encode, decode the data and generate license keys.'
 
 setup(
     name="stride-license",
     version=VERSION,
+    author="Sujith Dusa",
+    author_email="<sujith.kumar@stride.ai>",
     description=DESCRIPTION,
-    long_description=LONG_DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
     packages=find_packages(),
     install_requires=[
         "cryptography"
